ed426b1753b49447dc2cba715e0af16275337aad
Vitaliy <theyamshikov@yandex.ru>
2017-10-24 21:53:39 +0600

    refacto access

--- src/module/views/article/view.php
+++ src/module/views/article/view.php
@@ -7,23 +7,23 @@ use yii2module\guide\module\helpers\NavigationHelper;
 use yii2module\guide\module\helpers\ViewHelper;
 
 $this->title = $entity->title;
-$visibleClass = !Yii::$app->user->can('guide.modify') || $entity->project->readonly ? 'hidden' : '';
-?>
 
-<div class="pull-right <?= $visibleClass ?>">
-	<?= Html::a(Html::fa('pencil', ['class' => 'text-primary']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_UPDATE, ['id' => $entity->id]), [
-		'class' => 'btn btn-default',
-		'title' => t('action', 'UPDATE'),
-	]) ?>
-	<?= Html::a(Html::fa('trash', ['class' => 'text-danger']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_DELETE, ['id' => $entity->id]), [
-		'class' => 'btn btn-default',
-		'title' => t('action', 'DELETE'),
-		'data' => [
-			'confirm' => t('yii', 'Are you sure you want to delete this item?'),
-			'method' => 'post',
-		],
-	]) ?>
-</div>
+if(Yii::$app->user->can('guide.modify', $entity->project)) { ?>
+	<div class="pull-right">
+		<?= Html::a(Html::fa('pencil', ['class' => 'text-primary']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_UPDATE, ['id' => $entity->id]), [
+			'class' => 'btn btn-default',
+			'title' => t('action', 'UPDATE'),
+		]) ?>
+		<?= Html::a(Html::fa('trash', ['class' => 'text-danger']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_DELETE, ['id' => $entity->id]), [
+			'class' => 'btn btn-default',
+			'title' => t('action', 'DELETE'),
+			'data' => [
+				'confirm' => t('yii', 'Are you sure you want to delete this item?'),
+				'method' => 'post',
+			],
+		]) ?>
+	</div>
+<?php } ?>
 
 <?= ViewHelper::markdownToHtml($entity->content) ?>
 
