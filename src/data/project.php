c1754c0223cc6b28ff10d179f1c667edf79b17ed
Vitaliy <theyamshikov@yandex.ru>
2017-12-03 18:07:21 +0600

    move data

---
+++ src/data/project.php
@@ -0,0 +1,260 @@
+<?php 
+
+return [
+	
+	// --- main --- //
+	
+	[
+		'id' => 'application',
+		'dir' => 'common/docs',
+		'readonly' => false,
+	],
+	[
+		'id' => 'guide',
+		'dir' => 'vendor/yii2module/yii2-guide/guide/main/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'guide-mcconnell',
+		'dir' => 'vendor/yii2module/yii2-guide/guide/mcconnell/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'domain',
+		'dir' => 'vendor/yii2lab/yii2-domain/guide/ru',
+		'readonly' => false,
+	],
+	
+	// --- yii2lab --- //
+	
+	[
+		'id' => 'yii2lab.widgets',
+		'dir' => 'vendor/yii2lab/yii2-widgets/guide/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2lab.app',
+		'dir' => 'vendor/yii2lab/yii2-app/guide/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2lab.migration',
+		'dir' => 'vendor/yii2lab/yii2-migration/guide/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2lab.console',
+		'dir' => 'vendor/yii2lab/yii2-console/guide/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2lab.init',
+		'dir' => 'vendor/yii2lab/yii2-init/guide/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2lab.navigation',
+		'dir' => 'vendor/yii2lab/yii2-navigation/guide/ru',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2lab.notify',
+		'dir' => 'vendor/yii2lab/yii2-notify/guide/ru',
+		'readonly' => false,
+	],
+	
+	// --- yii2woop --- //
+	
+	[
+		'id' => 'yii2woop.account',
+		'dir' => 'vendor/yii2woop/yii2-account/guide/ru',
+		'readonly' => false,
+	],
+	
+	// --- yii2module --- //
+	
+	[
+		'id' => 'yii2module.fixture',
+		'dir' => 'vendor/yii2module/yii2-fixture',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2module.cleaner',
+		'dir' => 'vendor/yii2module/yii2-cleaner',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2module.environments',
+		'dir' => 'vendor/yii2module/yii2-environments',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2module.offline',
+		'dir' => 'vendor/yii2module/yii2-offline',
+		'readonly' => false,
+	],
+	[
+		'id' => 'yii2module.rbac',
+		'dir' => 'vendor/yii2module/yii2-rbac/guide/ru',
+		'readonly' => false,
+	],
+	
+	// --- thrid party --- //
+
+	/*[
+		'id' => 'yiisoft.yii2',
+		'dir' => 'vendor/yiisoft/yii2/docs/guide-ru',
+	],
+	[
+		'id' => 'yiisoft.apidoc',
+		'dir' => 'vendor/yiisoft/yii2-apidoc',
+	],
+	[
+		'id' => 'yiisoft.bootstrap',
+		'dir' => 'vendor/yiisoft/yii2-bootstrap',
+	],
+	[
+		'id' => 'yiisoft.composer',
+		'dir' => 'vendor/yiisoft/yii2-composer',
+	],
+	[
+		'id' => 'yiisoft.debug',
+		'dir' => 'vendor/yiisoft/yii2-debug',
+	],
+	[
+		'id' => 'yiisoft.gii',
+		'dir' => 'vendor/yiisoft/yii2-gii',
+	],
+	[
+		'id' => 'yiisoft.httpclient',
+		'dir' => 'vendor/yiisoft/yii2-httpclient',
+	],
+	[
+		'id' => 'yiisoft.imagine',
+		'dir' => 'vendor/yiisoft/yii2-imagine',
+	],
+	[
+		'id' => 'yiisoft.queue',
+		'dir' => 'vendor/yiisoft/yii2-queue',
+	],
+	[
+		'id' => 'yiisoft.swiftmailer',
+		'dir' => 'vendor/yiisoft/yii2-swiftmailer',
+	],
+
+	[
+		'id' => 'kartik-v.bootstrap-fileinput',
+		'dir' => 'vendor/kartik-v/bootstrap-fileinput',
+	],
+	[
+		'id' => 'kartik-v.dependent-dropdown',
+		'dir' => 'vendor/kartik-v/dependent-dropdown',
+	],
+
+	[
+		'id' => 'yii2mod.collection',
+		'dir' => 'vendor/yii2mod/collection',
+	],
+
+	[
+		'id' => 'symfony.finder',
+		'dir' => 'vendor/symfony/finder',
+	],
+
+	[
+		'id' => 'behat.gherkin',
+		'dir' => 'vendor/behat/gherkin',
+	],
+
+	[
+		'id' => 'guzzlehttp.guzzle',
+		'dir' => 'vendor/guzzlehttp/guzzle',
+	],
+
+	[
+		'id' => 'scrivo.highlight',
+		'dir' => 'vendor/scrivo/highlight.php',
+	],
+
+	[
+		'id' => 'imagine.imagine',
+		'dir' => 'vendor/imagine/imagine',
+	],
+
+	[
+		'id' => 'cebe.markdown',
+		'dir' => 'vendor/cebe/markdown',
+	],
+	[
+		'id' => 'cebe.markdown-latex',
+		'dir' => 'vendor/cebe/markdown-latex',
+	],
+
+	[
+		'id' => 'michelf.php-markdown',
+		'dir' => 'vendor/michelf/php-markdown',
+	],
+
+	[
+		'id' => 'symfony.process',
+		'dir' => 'vendor/symfony/process',
+	],
+
+	[
+		'id' => 'codeception.verify',
+		'dir' => 'vendor/codeception/verify',
+	],
+
+	[
+		'id' => 'sebastian.version',
+		'dir' => 'vendor/sebastian/version',
+	],
+
+	[
+		'id' => 'symfony.yaml',
+		'dir' => 'vendor/symfony/yaml',
+	],
+
+	[
+		'id' => 'dpodium.yii2-colorpicker',
+		'dir' => 'vendor/dpodium/yii2-colorpicker',
+	],
+
+	[
+		'id' => 'yii2mod.yii2-helpers',
+		'dir' => 'vendor/yii2mod/yii2-helpers',
+	],
+	[
+		'id' => 'paulzi.yii2-json-behavior',
+		'dir' => 'vendor/paulzi/yii2-json-behavior',
+	],
+
+	[
+		'id' => 'kartik-v.yii2-krajee-base',
+		'dir' => 'vendor/kartik-v/yii2-krajee-base',
+	],
+
+	[
+		'id' => 'kartik-v.yii2-markdown',
+		'dir' => 'vendor/kartik-v/yii2-markdown',
+	],
+
+	[
+		'id' => '2amigos.yii2-qrcode-helper',
+		'dir' => 'vendor/2amigos/yii2-qrcode-helper',
+	],
+
+	[
+		'id' => 'kartik-v.yii2-widget-activeform',
+		'dir' => 'vendor/kartik-v/yii2-widget-activeform',
+	],
+	[
+		'id' => 'kartik-v.yii2-widget-affix',
+		'dir' => 'vendor/kartik-v/yii2-widget-affix',
+	],
+	[
+		'id' => 'comodojo.zip',
+		'dir' => 'vendor/comodojo/zip',
+	],*/
+
+];
