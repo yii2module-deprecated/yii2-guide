1f718d477082120be545b52337aefecc6a1b4719
Vitaliy <theyamshikov@yandex.ru>
2017-12-10 14:21:28 +0600

    use markdown editor

--- src/module/views/article/update.php
+++ src/module/views/article/update.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @var $this yii\web\View
  * @var $model yii\base\Model
@@ -6,6 +7,7 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii2mod\markdown\MarkdownEditor;
 use yii2module\guide\module\widgets\Markdown;
 
 $this->title = t('action', 'UPDATE');
@@ -15,10 +17,30 @@ $this->title = t('action', 'UPDATE');
 <div class="row">
 	<div class="col-lg-12">
 		<?php $form = ActiveForm::begin(); ?>
-
-		<?= $form->field($model, 'content', ['template' => "{hint}\n{input}\n{error}"])->textarea(['rows' => 19, 'style' => 'font-family: Monaco, Menlo, Consolas, "Courier New", monospace']); ?>
-
-		<div class="form-group">
+		
+		<?= $form->field($model, 'content', ['template' => "{hint}\n{input}\n{error}"])->widget(MarkdownEditor::class, [
+			'editorOptions' => [
+				'showIcons' => ["code", "table"],
+                'toolbar' => [
+                    'bold', 'italic', 'strikethrough',
+                    '|',
+                    //'heading', 'heading-smaller', 'heading-bigger',
+                    'heading-1', 'heading-2', 'heading-3',
+	                '|',
+	                'code', 'quote',
+	                '|',
+                    'unordered-list', 'ordered-list',
+	                '|',
+	                'clean-block', 'link', 'image', 'table', 'horizontal-rule',
+	                '|',
+                    'preview', 'side-by-side', 'fullscreen', 'guide',
+	                
+                ],
+			],
+   
+		]); ?>
+        
+        <div class="form-group">
 			<?= Html::submitButton(t('action', 'SAVE'), ['class' => 'btn btn-primary']) ?>
 			<?= Html::submitButton(t('action', 'PREVIEW'), ['name'=> 'isPreview', 'value'=> '1', 'class' => 'btn btn-default']) ?>
 		</div>
@@ -26,9 +48,12 @@ $this->title = t('action', 'UPDATE');
 		<?php ActiveForm::end(); ?>
 	</div>
 
-	<div class="col-lg-12">
-		<?= Markdown::widget(['content' => $model->content]) ?>
-	</div>
+    <?php if(Yii::$app->request->isPost) { ?>
+        <div class="col-lg-12">
+		    <?= Markdown::widget(['content' => $model->content]) ?>
+        </div>
+	<?php } ?>
+	
 </div>
 
 <br/>
