8b206365c2519c9904f0a6adbdcd3f083c299fad
Vitaliy <theyamshikov@yandex.ru>
2017-12-17 03:03:09 +0600

    make domain

---
+++ src/domain/Domain.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace yii2module\guide\domain;
+
+use yii2lab\domain\enums\Driver;
+use yii2lab\domain\services\ActiveBaseService;
+
+class Domain extends \yii2lab\domain\Domain {
+	
+	public function config() {
+		return [
+			'repositories' => [
+				'project' => [
+					'class' => 'yii2module\guide\domain\repositories\file\ProjectRepository',
+					'owners' => [
+						'yii2lab',
+						'yii2module',
+						'yii2woop',
+					],
+				],
+				'article' => Driver::FILE,
+				'chapter' => Driver::FILE,
+			],
+			'services' => [
+				'project' => ActiveBaseService::className(),
+				'article',
+				'chapter',
+			],
+		];
+	}
+	
+}
