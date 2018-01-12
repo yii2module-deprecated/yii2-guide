48541fba69f9659d56a59bc96dfcbda9248c2584
Vitaliy <theyamshikov@yandex.ru>
2018-01-13 01:43:19 +0600

    refacto

--- src/module/views/article/view.php
+++ src/module/views/article/view.php
@@ -4,27 +4,39 @@
 
 use yii2lab\helpers\yii\Html;
 use yii2module\guide\module\helpers\NavigationHelper;
+use yii2module\markdown\widgets\helpers\ArticleMenuHelper;
+use yii2module\markdown\widgets\helpers\MarkdownHelper;
 use yii2module\markdown\widgets\Markdown;
 
 $this->title = $entity->title;
+
+$html = Markdown::widget(['content' => $entity->content]);
+
+if(!$entity->is_main) {
+	$menu = ArticleMenuHelper::getMenuFromMarkdown($entity->content);
+	if(!empty($menu)) {
+		$html = ArticleMenuHelper::addIdInHeaders($html);
+	}
+	$menuMd = ArticleMenuHelper::makeMenuMd($menu);
+	$menuHtml = MarkdownHelper::toHtml($menuMd);
+	$html = str_replace('</h1>', '</h1>' . $menuHtml . PHP_EOL, $html);
+}
+
 ?>
 
 <div class="pull-right">
 
 <?php
 echo Html::a(Html::fa('code', ['class' => 'text-primary']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_CODE, ['id' => $entity->id]), [
-	//'class' => 'btn btn-default',
 	'title' => Yii::t('action', 'code'),
 ]);
 echo NBSP;
 if(Yii::$app->user->can('guide.modify', $entity->project)) {
 	echo Html::a(Html::fa('pencil', ['class' => 'text-primary']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_UPDATE, ['id' => $entity->id]), [
-		//'class' => 'btn btn-default',
 		'title' => Yii::t('action', 'update'),
 	]);
 	echo NBSP;
 	echo Html::a(Html::fa('trash', ['class' => 'text-danger']), NavigationHelper::genUrl(NavigationHelper::URL_ARTICLE_DELETE, ['id' => $entity->id]), [
-		//'class' => 'btn btn-default',
 		'title' => Yii::t('action', 'delete'),
 		'data' => [
 			'confirm' => Yii::t('yii', 'Are you sure you want to delete this item?'),
@@ -35,6 +47,6 @@ if(Yii::$app->user->can('guide.modify', $entity->project)) {
 
 </div>
 
-<?= Markdown::widget(['content' => $entity->content]) ?>
+<?= $html ?>
 
 <br/>
